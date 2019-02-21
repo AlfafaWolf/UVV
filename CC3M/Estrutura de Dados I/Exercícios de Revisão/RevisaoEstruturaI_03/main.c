739efe67c11f76dfa235476c8fcd6bac98bcc36c
Gabriel Alejandro L. M <35781604+AlfafaWolf@users.noreply.github.com>
2019-02-20 23:14:44 -0300

    Finalização do Exercício 3 de Revisão

--- CC3M/Estrutura de Dados I/Exercícios de Revisão/RevisaoEstruturaI_03/main.c
+++ CC3M/Estrutura de Dados I/Exercícios de Revisão/RevisaoEstruturaI_03/main.c
@@ -1,3 +1,4 @@
+// Gabriel Alejandro L. M. - CC3M
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -6,27 +7,38 @@ typedef struct Tponto
 {
     TipoP x, y, z;
 }*Ponto;
+TipoP getX(Ponto pto);
+TipoP getY(Ponto pto);
+TipoP getZ(Ponto pto);
+void setX(Ponto pto, TipoP x);
+void setY(Ponto pto, TipoP y);
+void setZ(Ponto pto, TipoP z);
 Ponto CriaPonto(TipoP x, TipoP y, TipoP z);
 float distEntrePtos(Ponto p1, Ponto p2);
 Ponto centroGeom (Ponto *vetPtos, int nPtos);
+int pontosIguais(Ponto p1, Ponto p2);
+Ponto somaPontos(Ponto p1, Ponto p2);
+Ponto difPontos(Ponto p1, Ponto p2);
+float calculaPerimetroQuadrilatero(Ponto *vetPto, int n);
+Ponto pontoMedio(Ponto p1, Ponto p2);
 int main()
 {
+    // Criando Pontos A e B
     Ponto A = NULL;
     A = CriaPonto(0, 0, 0);
-
     Ponto B = NULL;
     B = CriaPonto(1, 1, 1);
 
     // Ponto A
     printf("Ponto A:\n");
     printf("   X   |   Y   |   Z   |\n");
-    printf("%7.2f|%7.2f|%7.2f|\n", A->x, A->y, A->z);
+    printf("%7.2f|%7.2f|%7.2f|\n", getX(A), getY(A), getZ(A));
     printf("Ponto A foi alocado no endereco %p\n\n", A);
 
     // Ponto B
     printf("Ponto B:\n");
     printf("   X   |   Y   |   Z   |\n");
-    printf("%7.2f|%7.2f|%7.2f|\n", B->x, B->y, B->z);
+    printf("%7.2f|%7.2f|%7.2f|\n", getX(B), getY(B), getZ(B));
     printf("Ponto B foi alocado no endereco %p\n\n", B);
 
     // Calcular Distancia entre A e B
@@ -36,10 +48,8 @@ int main()
     // Criar Pontos do Triangulo
     Ponto C = NULL;
     C = CriaPonto(0, 0, 0);
-
     Ponto D = NULL;
     D = CriaPonto(1, 0, 0);
-
     Ponto E = NULL;
     E = CriaPonto(0, 1, 0);
 
@@ -53,11 +63,44 @@ int main()
     Ponto O = NULL;
     O = centroGeom(vetPontos, 3);
 
-    // Ponto B
+    // Ponto O
     printf("Centro Geometrico O do triangulo CDE:\n");
     printf("   X   |   Y   |   Z   |\n");
-    printf("%7.2f|%7.2f|%7.2f|\n", O->x, O->y, O->z);
-    printf("Ponto B foi alocado no endereco %p\n\n", O);
+    printf("%7.2f|%7.2f|%7.2f|\n", getX(O), getY(O), getZ(O));
+    printf("Ponto O foi alocado no endereco %p\n\n", O);
+
+    // Pontos Iguais
+    int pIgual = pontosIguais(A, B);
+    if(pIgual != 0)
+        printf("Os pontos A e B sao iguais\n\n");
+    else
+        printf("Os pontos A e B sao diferentes\n\n");
+
+    // Definindo Pontos
+    Ponto p1, p2, p3, p4;
+    p1 = CriaPonto(0,0,0);
+    p2 = CriaPonto(0,1,0);
+    p3 = CriaPonto(1,1,0);
+    p4 = CriaPonto(1,0,0);
+
+    // Perimetro Quadrilatero
+    Ponto vetQuadrilatero[4];
+    float perimetro;
+
+    vetQuadrilatero[0] = p1;
+    vetQuadrilatero[1] = p2;
+    vetQuadrilatero[2] = p3;
+    vetQuadrilatero[3] = p4;
+
+    perimetro = calculaPerimetroQuadrilatero(vetQuadrilatero, 4);
+    printf("Perimetro do Quadrilatero = %f\n\n", perimetro);
+
+    // Ponto Medio
+    Ponto pMedio = NULL;
+    pMedio = pontoMedio(p1, p2);
+    printf("Ponto Medio de p1 e p2:\n");
+    printf("   X   |   Y   |   Z   |\n");
+    printf("%7.2f|%7.2f|%7.2f|\n", getX(pMedio), getY(pMedio), getZ(pMedio));
 
     // Desalocar da memória
     free(A);
@@ -66,6 +109,11 @@ int main()
     free(D);
     free(E);
     free(O);
+    free(p1);
+    free(p2);
+    free(p3);
+    free(p4);
+    free(pMedio);
 
     return 0;
 }
@@ -97,9 +145,9 @@ Ponto centroGeom(Ponto *vetPtos, int nPtos)
         printf("Erro, nao foi possivel alocar");
         exit(1);
     }
-    p->x = 0;
-    p->y = 0;
-    p->z = 0;
+    setX(p, 0);
+    setY(p, 0);
+    setZ(p, 0);
 
     int i;
     for(i = 0; i < nPtos; i++)
@@ -113,4 +161,94 @@ Ponto centroGeom(Ponto *vetPtos, int nPtos)
     p->z /= nPtos;
 
     return p;
+}
+int pontosIguais(Ponto p1, Ponto p2)
+{
+    return (p1->x == p2->x && p1->y == p2->y && p1->z == p2->z);
+}
+Ponto somaPontos(Ponto p1, Ponto p2)
+{
+    Ponto p = NULL;
+    p = (Ponto) malloc(sizeof(struct Tponto));
+    if(p == NULL)
+    {
+        printf("Erro, nao foi possivel alocar");
+        exit(1);
+    }
+    setX(p, getX(p1) + getX(p2));
+    setY(p, getY(p1) + getY(p2));
+    setZ(p, getZ(p1) + getZ(p2));
+
+    return p;
+}
+Ponto difPontos(Ponto p1, Ponto p2)
+{
+    Ponto p = NULL;
+    p = (Ponto) malloc(sizeof(struct Tponto));
+    if(p == NULL)
+    {
+        printf("Erro, nao foi possivel alocar");
+        exit(1);
+    }
+    setX(p, getX(p1) - getX(p2));
+    setY(p, getY(p1) - getY(p2));
+    setZ(p, getZ(p1) - getZ(p2));
+
+    return p;
+}
+float calculaPerimetroQuadrilatero(Ponto *vetPto, int n)
+{
+    float dist = 0;
+    int i;
+    for(i = 0; i < n; i++)
+    {
+        if(i + 1 < n)
+        {
+            dist += distEntrePtos(vetPto[i], vetPto[i + 1]);
+        }
+        else
+        {
+            dist += distEntrePtos(vetPto[i], vetPto[0]);
+        }
+    }
+    return dist;
+}
+Ponto pontoMedio(Ponto p1, Ponto p2)
+{
+    Ponto pMedio = NULL;
+    pMedio = (Ponto) malloc(sizeof(Ponto));
+    if(pMedio == NULL)
+    {
+        printf("Erro, nao foi possivel alocar");
+        exit(1);
+    }
+    pMedio->x = (p1->x + p2->x) / 2;
+    pMedio->y = (p1->y + p2->y) / 2;
+    pMedio->z = (p1->z + p2->z) / 2;
+
+    return pMedio;
+}
+TipoP getX(Ponto pto)
+{
+    return pto->x;
+}
+TipoP getY(Ponto pto)
+{
+    return pto->y;
+}
+TipoP getZ(Ponto pto)
+{
+    return pto->z;
+}
+void setX(Ponto pto, TipoP x)
+{
+    pto->x = x;
+}
+void setY(Ponto pto, TipoP y)
+{
+    pto->y = y;
+}
+void setZ(Ponto pto, TipoP z)
+{
+    pto->z = z;
 }
